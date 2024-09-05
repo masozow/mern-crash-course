8c4e3f56c0a9aee1e6c6e16fd3723d38bc2d9f7e
masozow <14838605+masozow@users.noreply.github.com>
2024-09-04 23:58:12 -0600

    creating and endpoint to create a new product

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
-
+import Product from '../models/product.model.js';
 
 const app = express();
+app.use(express.json()); // allows us to accept JSON data in body
 
 app.get('/', (req, res) => {
     res.send('Server is ready!');
+});
+
+app.post('/api/products', async (req, res) => {
+    const product = req.body;
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error("Error in Create product: ", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
 });
 
 app.listen(5000, () => {
